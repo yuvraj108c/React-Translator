910b64cbeb418c964a980af7216439236388ac02
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-06-17 16:53:45 +0400

    Added formatted message

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "../styles/Home.css";
 
+import { FormattedMessage } from "react-intl";
+
 const Home = () => {
   return (
     <div className="home">
       <div className="main">
         <div className="hero">
-          <h2 className="title">Welcome!</h2>
-          <p className="sub">This is our sub heading</p>
-          <button>Get started</button>
+          <h2 className="greeting">
+            <FormattedMessage id="home.greeting" />
+          </h2>
+          <p className="description">
+            <FormattedMessage id="home.description" />
+          </p>
+          <button>
+            <FormattedMessage id="home.button" />
+          </button>
         </div>
       </div>
     </div>
