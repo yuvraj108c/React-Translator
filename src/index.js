960e852aa39eaf4c8e6d25263c5243939be1e991
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-06-17 16:54:08 +0400

    Change language based on global state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+import { IntlProvider } from "react-intl";
+import { addLocaleData } from "react-intl";
+import locale_en from "react-intl/locale-data/en";
+import locale_fr from "react-intl/locale-data/fr";
+
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./store/reducer";
@@ -9,17 +14,34 @@ import reducer from "./store/reducer";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import messages_en from "./translations/en.json";
+import messages_fr from "./translations/fr.json";
+
+addLocaleData([...locale_en, ...locale_fr]);
+
+const messages = {
+  en: messages_en,
+  fr: messages_fr
+};
+
 const store = createStore(reducer);
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const language = store.getState().language;
+  renderIndex(language);
 });
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const renderIndex = language => {
+  ReactDOM.render(
+    <IntlProvider locale={language} messages={messages[language]}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </IntlProvider>,
+    document.getElementById("root")
+  );
+};
+
+renderIndex(store.getState().language);
 
 serviceWorker.unregister();
