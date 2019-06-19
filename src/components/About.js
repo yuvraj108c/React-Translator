b90e606f4b1638fbffb3c436976ac3391c7ad1f1
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-06-19 11:15:32 +0400

    Added jumbotron

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,26 @@
 import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 import { FormattedMessage } from "react-intl";
 
 const About = () => {
   return (
-    <div className="about">
-      <h3>
-        <FormattedMessage id="about.message" />
-      </h3>
-    </div>
+    <Jumbotron>
+      <Container>
+        <h1>
+          <FormattedMessage id="about.title" />
+        </h1>
+        <p>
+          <FormattedMessage id="about.message" />
+        </p>
+        <br />
+        <hr />
+        <Button>
+          <FormattedMessage id="about.button" />
+        </Button>
+      </Container>
+    </Jumbotron>
   );
 };
 
