ffd642edf38450ea13ea2509508ece35b3f226a6
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-06-20 14:52:58 +0400

    Fixed bug with empty default language

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,7 @@ const { origin, href } = window.location;
 const language = href.replace(origin, "").slice(1, 3);
 
 const initialState = {
-  language: language
+  language: language || "en"
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
