81a1f3b6ea63eff1d67185b02b31d879b5fc6a66
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-06-20 14:32:48 +0400

    Changed intital language to url language

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,8 @@
+const { origin, href } = window.location;
+const language = href.replace(origin, "").slice(1, 3);
+
 const initialState = {
-  language: "en"
+  language: language
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
