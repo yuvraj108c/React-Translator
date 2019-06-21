7b34ac329bfbcb21b8e5573f67d0289d2b17d49c
yuvraj108c <yuvraj.seegolam@umail.uom.ac.mu>
2019-06-21 10:51:58 +0400

    Added cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,35 +6,98 @@ import food from "../assets/img/food.jpg";
 import dinner from "../assets/img/dinner.jpg";
 import { FormattedMessage } from "react-intl";
 
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+
+import chef from "../assets/img/chef.jpg";
+import food2 from "../assets/img/local-food.jpg";
+import customer from "../assets/img/customer.jpg";
+
 const Home = () => {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <div className="image-container">
-          <img className="d-block w-100" src={food} alt="First slide" />
-        </div>
-        <Carousel.Caption>
-          <h1>
-            <FormattedMessage id="carousel.image1.title" />
-          </h1>
-          <p>
-            <FormattedMessage id="carousel.image1.description" />
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={dinner} alt="Third slide" />
+    <div className="home">
+      <Carousel>
+        <Carousel.Item>
+          <div className="image-container">
+            <img className="d-block w-100" src={food} alt="First slide" />
+          </div>
+          <Carousel.Caption>
+            <h1>
+              <FormattedMessage id="carousel.image1.title" />
+            </h1>
+            <p>
+              <FormattedMessage id="carousel.image1.description" />
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={dinner} alt="Third slide" />
 
-        <Carousel.Caption>
-          <h1>
-            <FormattedMessage id="carousel.image2.title" />
-          </h1>
-          <p>
-            <FormattedMessage id="carousel.image2.description" />
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          <Carousel.Caption>
+            <h1>
+              <FormattedMessage id="carousel.image2.title" />
+            </h1>
+            <p>
+              <FormattedMessage id="carousel.image2.description" />
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <Container>
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src={chef} />
+            <Card.Body>
+              <Card.Title>
+                <FormattedMessage id="home.card1.title" />
+              </Card.Title>
+              <Card.Text>
+                <FormattedMessage id="home.card1.text" />
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Card.Link href="#">
+                <FormattedMessage id="home.card.footer" />
+              </Card.Link>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={food2} />
+            <Card.Body>
+              <Card.Title>
+                <FormattedMessage id="home.card2.title" />
+              </Card.Title>
+              <Card.Text>
+                <FormattedMessage id="home.card2.text" />
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Card.Link href="#">
+                <FormattedMessage id="home.card.footer" />
+              </Card.Link>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src={customer} />
+            <Card.Body>
+              <Card.Title>
+                <FormattedMessage id="home.card3.title" />
+              </Card.Title>
+              <Card.Text>
+                <FormattedMessage id="home.card3.text" />
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Card.Link href="#">
+                <FormattedMessage id="home.card.footer" />
+              </Card.Link>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
+      </Container>
+    </div>
   );
 };
 
